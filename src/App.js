3e1e5dc2a551d21bda71503e9b5cabcab13b66c4
Adrianderson Lira <adriandersonmusica@gmail.com>
2020-06-23 23:38:00 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `Repository ${Date.now()}`,
+      url: "http://github.com",
+      techs: ["ReactJS"]
+    });
+
+    setRepositories([...repositories, response.data]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id))
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map( repository => (
+          <li>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+        
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
